ab72435bda49a24db1b57b0d72794d4307935018
Pedro Duarte <contact@peduarte.com>
2019-07-09 13:40:11 +0100

    test: testing primitive API

--- packages/primitives/src/components/Button.story.tsx
+++ packages/primitives/src/components/Button.story.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, select } from '@storybook/addon-knobs/react';
+import 'styled-components/macro';
 import { Button } from './Button';
 import { theme } from '../theme';
+import { css as sx } from '@styled-system/css';
+import { variant } from '@modulz/radix-system';
 
 const options = ops => ({ '': 'null', ...ops });
 const spaceKnob = label => select(label, options(theme.space));
@@ -18,7 +21,7 @@ storiesOf('Components|Button', module)
   .addDecorator(withKnobs)
   .add('default', () => (
     <>
-      <Button
+      {/* <Button
         padding={spaceKnob('Padding')}
         paddingTop={spaceKnob('Padding top')}
         paddingRight={spaceKnob('Padding right')}
@@ -37,6 +40,78 @@ storiesOf('Components|Button', module)
         lineHeight={lineHeightKnob('Line height')}
       >
         Button
-      </Button>
+      </Button> */}
+
+      {/* <Button
+        variant={select('variant', ['primary', 'danger'])}
+        marginTop={spaceKnob('Margin top')}
+        css={[
+          sx({
+            backgroundColor: 'blue400',
+            '&:hover': {
+              backgroundColor: 'blue600',
+            },
+          }),
+          variant({
+            variant: {
+              primary: {
+                color: 'blue200',
+              },
+              danger: {
+                backgroundColor: 'red400',
+                color: 'red200',
+              },
+            },
+          }),
+        ]}
+      >
+        Hello
+      </Button> */}
+      <RadixButton>Hello</RadixButton>
+      <RadixButton size="medium">Hello</RadixButton>
+      <RadixButton variant="danger" ml={8}>
+        Hello
+      </RadixButton>
+      <RadixButton variant="danger" size="medium" ml={8}>
+        Hello
+      </RadixButton>
     </>
   ));
+
+function RadixButton(props) {
+  return (
+    <Button
+      {...props}
+      css={[
+        sx({
+          backgroundColor: 'blue400',
+          ':hover': {
+            backgroundColor: 'blue600',
+          },
+        }),
+        variant({
+          variant: {
+            primary: {
+              color: 'blue200',
+            },
+            danger: {
+              backgroundColor: 'red400',
+              color: 'red200',
+              '&:hover': {
+                backgroundColor: 'red600',
+              },
+            },
+          },
+        }),
+        variant({
+          size: {
+            small: {},
+            medium: {
+              paddingY: 3,
+            },
+          },
+        }),
+      ]}
+    />
+  );
+}
