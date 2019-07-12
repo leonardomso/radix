ec21dd7cfda590478a3b227aedfd19936b5d4de8
Benoît Grélard <benoit.grelard@gmail.com>
2019-07-12 09:32:12 +0100

    test(primitives): add test setup and Portal tests

---
+++ packages/primitives/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: 'ts-jest',
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+    '@testing-library/react/cleanup-after-each',
+  ],
+};
---
+++ packages/primitives/src/components/__tests__/Portal.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Portal } from '../Portal';
+
+describe('Portal', () => {
+  test('render', () => {
+    const { baseElement } = render(<Portal>portal</Portal>);
+    expect(baseElement).toMatchInlineSnapshot(`
+      <body>
+        <div />
+        <radix-portal>
+          portal
+        </radix-portal>
+      </body>
+    `);
+  });
+
+  test('render sibling', () => {
+    const { baseElement } = render(
+      <>
+        <Portal>portal 1</Portal>
+        <Portal>portal 2</Portal>
+      </>
+    );
+    expect(baseElement).toMatchInlineSnapshot(`
+      <body>
+        <div />
+        <radix-portal>
+          portal 1
+        </radix-portal>
+        <radix-portal>
+          portal 2
+        </radix-portal>
+      </body>
+    `);
+  });
+
+  test('cleanup', () => {
+    const { baseElement, unmount } = render(<Portal>portal</Portal>);
+    unmount();
+    expect(baseElement).toMatchInlineSnapshot(`
+      <body>
+        <div />
+      </body>
+    `);
+  });
+});
