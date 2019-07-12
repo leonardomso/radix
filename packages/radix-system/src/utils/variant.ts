bd5a18d0bcfed2d404aacc119b5e4edd30a9cd16
Pedro Duarte <contact@peduarte.com>
2019-07-12 10:47:15 +0100

    feat(radix-system): update `variant` API to support multiple keys

--- packages/radix-system/src/utils/variant.ts
+++ packages/radix-system/src/utils/variant.ts
@@ -1,14 +1,17 @@
 import { get, createParser } from '@styled-system/core';
 import css from '@styled-system/css';
 
+// @ts-ignore
+const getValue = (n, scale) => get(scale, n);
+
 export const variant = (config: any) => (props: any) => {
-  const prop = Object.keys(config)[0];
-  // @ts-ignore
-  const getValue = (n, scale) => get(scale, n);
-  getValue.defaults = config[prop];
-  return css(
-    createParser({
-      [prop]: getValue,
-    })(props)
-  );
+  return Object.keys(config).map(prop => {
+    // @ts-ignore
+    getValue.defaults = config[prop];
+    return css(
+      createParser({
+        [prop]: getValue,
+      })(props)
+    );
+  });
 };
